d2cc8c722c7ab8d5fe5f819be6df875040b012c4
kozhini <131589545+kozhini@users.noreply.github.com>
2026-06-13 22:44:49 +0700

    Update amneziawg.js

--- luci-proto-amneziawg/htdocs/luci-static/resources/protocol/amneziawg.js
+++ luci-proto-amneziawg/htdocs/luci-static/resources/protocol/amneziawg.js
@@ -882,7 +882,7 @@ return network.registerProtocol('amneziawg', {
 
 				var qrm, qrs, qro;
 
-				qrm = new form.JSONMap({ config: { endpoint: hostnames[0], allowed_ips: ips, addresses: eips, dns_servers: dns } }, null, _('The generated configuration can be imported into a WireGuard client application to set up a connection towards this device.'));
+				qrm = new form.JSONMap({ config: { endpoint: hostnames[0] || '', allowed_ips: ips, addresses: eips, dns_servers: dns } }, null, _('The generated configuration can be imported into a WireGuard client application to set up a connection towards this device.'));
 				qrm.parent = parent;
 
 				qrs = qrm.section(form.NamedSection, 'config');
